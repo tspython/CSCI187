7ba787b61e7f3368d2c6b3262197641b73d29327
Bennett Nguyen <bennett.nguyen@gmail.com>
2023-10-29 18:36:55 -0700

    started dashboard

--- App/DashboardScreen.js
+++ App/DashboardScreen.js
@@ -1,5 +1,52 @@
+// import React, { useEffect, useState } from 'react';
+// import { View, Text } from 'react-native';
+// import axios from 'axios';
+// import AsyncStorage from '@react-native-community/async-storage';
+// import { useNavigation } from '@react-navigation/native';
+
+// const DashboardScreen = () => {
+//   const navigation = useNavigation();
+//   const [user, setUser] = useState(null);
+
+//   useEffect(() => {
+//     const checkToken = async () => {
+//       const token = await AsyncStorage.getItem('authToken');
+//       if (!token) {
+//         // If there's no token, navigate back to the login screen
+//         navigation.navigate('Login');
+//       } else {
+//         // If there is a token, make an authenticated request to get user data
+//         try {
+//           const response = await axios.get('http://localhost:3000/dashboard', {
+//             headers: {
+//               Authorization: `Bearer ${token}`,
+//             },
+//           });
+//           setUser(response.data);
+//         } catch (error) {
+//           console.error(error);
+//           // Handle authentication error here
+//           // Example: navigate to the login screen or show an error message
+//           navigation.navigate('Login');
+//         }
+//       }
+//     };
+
+//     checkToken();
+//   }, [navigation]);
+
+//   return (
+//     <View>
+//       <Text>Welcome to your dashboard!</Text>
+//       {user && <Text>Email: {user.email}</Text>}
+//     </View>
+//   );
+// };
+
+// export default DashboardScreen;
+
 import React, { useEffect, useState } from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, Button, StyleSheet } from 'react-native';
 import axios from 'axios';
 import AsyncStorage from '@react-native-community/async-storage';
 import { useNavigation } from '@react-navigation/native';
@@ -12,21 +59,17 @@ const DashboardScreen = () => {
     const checkToken = async () => {
       const token = await AsyncStorage.getItem('authToken');
       if (!token) {
-        // If there's no token, navigate back to the login screen
         navigation.navigate('Login');
       } else {
-        // If there is a token, make an authenticated request to get user data
         try {
           const response = await axios.get('http://localhost:3000/dashboard', {
             headers: {
-              Authorization: `Bearer ${token}`,
+              Authorization: Bearer ${token},
             },
           });
           setUser(response.data);
         } catch (error) {
           console.error(error);
-          // Handle authentication error here
-          // Example: navigate to the login screen or show an error message
           navigation.navigate('Login');
         }
       }
@@ -35,13 +78,38 @@ const DashboardScreen = () => {
     checkToken();
   }, [navigation]);
 
+  const handleLogout = async () => {
+    await AsyncStorage.removeItem('authToken');
+    navigation.navigate('Login');
+  };
+
   return (
-    <View>
-      <Text>Welcome to your dashboard!</Text>
-      {user && <Text>Email: {user.email}</Text>}
+    <View style={styles.container}>
+      <Text style={styles.title}>Welcome to your dashboard!</Text>
+      {user && <Text style={styles.userInfo}>Email: {user.email}</Text>}
+      <Button title="Search Routes" onPress={() => navigation.navigate('RouteSearch')} />
+      <Button title="Logout" onPress={handleLogout} />
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    padding: 20,
+  },
+  title: {
+    fontSize: 24,
+    textAlign: 'center',
+    marginBottom: 20,
+  },
+  userInfo: {
+    fontSize: 18,
+    marginBottom: 20,
+    textAlign: 'center',
+  },
+});
 
 export default DashboardScreen;
 
