27a509536ecd122de02e39a5205cd523bd797c7c
skak122 <71853125+skak122@users.noreply.github.com>
2023-10-29 18:03:55 -0700

    Update LoginScreen.js (#6)
    I updated the Login screen

--- App/LoginScreen.js
+++ App/LoginScreen.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { View, Text, TextInput, Button } from 'react-native';
+import { View, Text, TextInput, Button, StyleSheet, Alert } from 'react-native';
 import axios from 'axios';
 import AsyncStorage from '@react-native-community/async-storage';
 import { useNavigation } from '@react-navigation/native';
@@ -8,37 +8,42 @@ const LoginScreen = () => {
   const navigation = useNavigation();
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const [error, setError] = useState(null);
 
   const handleLogin = async () => {
+    if (!email || !password) {
+      setError('Please fill in both email and password.');
+      return;
+    }
+
     try {
       const response = await axios.post('http://localhost:3000/login', {
         email: email,
         password: password,
       });
-      
-      const token = response.data.token; // Assuming the server returns a token
-      // Save the token to AsyncStorage or any other secure storage method
-      // Example using AsyncStorage:
-      await AsyncStorage.setItem('authToken', token);
 
-      console.log(response.data);
+      const token = response.data.token; // Assuming the server returns a token
+      await AsyncStorage.setItem('authToken', token); // Save token for future authentication
+
       navigation.navigate('Dashboard'); // Navigate to the Dashboard screen
     } catch (error) {
       console.error(error);
-      // Handle login error here
-      // Example: show an error message
+      setError('Login failed. Please check your credentials.');
     }
   };
 
   return (
-    <View>
-      <Text>Login Screen</Text>
+    <View style={styles.container}>
+      <Text style={styles.title}>Login Screen</Text>
+      {error && <Text style={styles.errorText}>{error}</Text>}
       <TextInput
+        style={styles.input}
         placeholder="Email"
         value={email}
         onChangeText={setEmail}
       />
       <TextInput
+        style={styles.input}
         placeholder="Password"
         secureTextEntry
         value={password}
@@ -48,6 +53,32 @@ const LoginScreen = () => {
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    padding: 20,
+  },
+  title: {
+    fontSize: 24,
+    textAlign: 'center',
+    marginBottom: 20,
+  },
+  input: {
+    height: 40,
+    borderColor: 'gray',
+    borderWidth: 1,
+    marginBottom: 10,
+    paddingHorizontal: 10,
+    borderRadius: 5,
+  },
+  errorText: {
+    color: 'red',
+    marginBottom: 10,
+    textAlign: 'center',
+  },
+});
 
 export default LoginScreen;
 
