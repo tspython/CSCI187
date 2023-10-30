e6c21de0769543cac9ee96c00418db8647149120
Bennett Nguyen <bennett.nguyen@gmail.com>
2023-10-29 19:35:43 -0700

    added functionality and added styling

--- App/DashboardScreen.js
+++ App/DashboardScreen.js
@@ -1,52 +1,6 @@
-// import React, { useEffect, useState } from 'react';
-// import { View, Text } from 'react-native';
-// import axios from 'axios';
-// import AsyncStorage from '@react-native-community/async-storage';
-// import { useNavigation } from '@react-navigation/native';
-
-// const DashboardScreen = () => {
-//   const navigation = useNavigation();
-//   const [user, setUser] = useState(null);
-
-//   useEffect(() => {
-//     const checkToken = async () => {
-//       const token = await AsyncStorage.getItem('authToken');
-//       if (!token) {
-//         // If there's no token, navigate back to the login screen
-//         navigation.navigate('Login');
-//       } else {
-//         // If there is a token, make an authenticated request to get user data
-//         try {
-//           const response = await axios.get('http://localhost:3000/dashboard', {
-//             headers: {
-//               Authorization: `Bearer ${token}`,
-//             },
-//           });
-//           setUser(response.data);
-//         } catch (error) {
-//           console.error(error);
-//           // Handle authentication error here
-//           // Example: navigate to the login screen or show an error message
-//           navigation.navigate('Login');
-//         }
-//       }
-//     };
-
-//     checkToken();
-//   }, [navigation]);
-
-//   return (
-//     <View>
-//       <Text>Welcome to your dashboard!</Text>
-//       {user && <Text>Email: {user.email}</Text>}
-//     </View>
-//   );
-// };
-
-// export default DashboardScreen;
-
 import React, { useEffect, useState } from 'react';
-import { View, Text, Button, StyleSheet } from 'react-native';
+import { View, Text, Button, StyleSheet, TouchableOpacity } from 'react-native';
+import MapView, { PROVIDER_GOOGLE } from 'react-native-maps';
 import axios from 'axios';
 import AsyncStorage from '@react-native-community/async-storage';
 import { useNavigation } from '@react-navigation/native';
@@ -64,7 +18,7 @@ const DashboardScreen = () => {
         try {
           const response = await axios.get('http://localhost:3000/dashboard', {
             headers: {
-              Authorization: Bearer ${token},
+              Authorization: `Bearer ${token}`,
             },
           });
           setUser(response.data);
@@ -87,8 +41,29 @@ const DashboardScreen = () => {
     <View style={styles.container}>
       <Text style={styles.title}>Welcome to your dashboard!</Text>
       {user && <Text style={styles.userInfo}>Email: {user.email}</Text>}
-      <Button title="Search Routes" onPress={() => navigation.navigate('RouteSearch')} />
-      <Button title="Logout" onPress={handleLogout} />
+
+      <MapView
+        provider={PROVIDER_GOOGLE} // Use Google Maps
+        style={styles.map}
+        initialRegion={{
+          latitude: 37.78825,
+          longitude: -122.4324,
+          latitudeDelta: 0.0922,
+          longitudeDelta: 0.0421,
+        }}
+      />
+
+      <View style={styles.buttonContainer}>
+        <TouchableOpacity
+          style={styles.routeButton}
+          onPress={() => navigation.navigate('RouteSearch')}
+        >
+          <Text style={styles.buttonText}>Search Routes</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.logoutButton} onPress={handleLogout}>
+          <Text style={styles.buttonText}>Logout</Text>
+        </TouchableOpacity>
+      </View>
     </View>
   );
 };
@@ -109,7 +84,33 @@ const styles = StyleSheet.create({
     marginBottom: 20,
     textAlign: 'center',
   },
+  map: {
+    flex: 1,
+    marginTop: 20,
+    borderRadius: 10,
+  },
+  buttonContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    marginTop: 20,
+  },
+  routeButton: {
+    backgroundColor: '#007AFF',
+    paddingVertical: 10,
+    paddingHorizontal: 20,
+    borderRadius: 5,
+  },
+  logoutButton: {
+    backgroundColor: '#FF3B30',
+    paddingVertical: 10,
+    paddingHorizontal: 20,
+    borderRadius: 5,
+  },
+  buttonText: {
+    color: '#fff',
+    fontSize: 16,
+    textAlign: 'center',
+  },
 });
 
 export default DashboardScreen;
-
