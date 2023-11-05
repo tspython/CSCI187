08e549574a223496ebb24ab81365921c4966abe5
Bennett Nguyen <bennett.nguyen@gmail.com>
2023-11-04 23:26:52 -0700

    added next step functions, beautified

--- App/Navigation.js
+++ App/Navigation.js
@@ -10,6 +10,16 @@ const NavigationPage = ({ navigation }) => {
 
   // Add handlers for opening preferences, setting locations, etc.
 
+  const openPreferences = () => {
+    // Open preferences modal or navigate to preferences page
+    navigation.navigate('PreferencesScreen');
+  }
+
+  const getPreferences = () => {
+    // Get the user's preferences
+    // For example, get the user's preferred mode of transportation
+  }
+
   const handleSearch = async () => {
     if (!startLocation || !destination) {
       setError('Please fill in both start and destination fields');
@@ -21,9 +31,11 @@ const NavigationPage = ({ navigation }) => {
     try {
       // Perform API calls to get routes and preferences
       // For example, get the coordinates for start and destination
+      const response = await fetch(`https://api.example.com/routes?start=${startLocation}&end=${destination}`);
+      const data = await response.json();
 
       // Navigate to results passing the necessary data
-      navigation.navigate('ResultsPage', { /* data */ });
+      navigation.navigate('ResultsPage', { routes: data.routes });
     } catch (error) {
       setError(error.message);
     } finally {
@@ -32,27 +44,53 @@ const NavigationPage = ({ navigation }) => {
   };
 
   return (
-    <View>
-      {isLoading && <ActivityIndicator size="large" />}
-      <Text>{error}</Text>
+    <View style={styles.container}>
+      {isLoading && <ActivityIndicator size="large" color="#007AFF" />}
+      <Text style={styles.errorText}>{error}</Text>
       <TextInput
+        style={styles.input}
         placeholder="Start Location"
         value={startLocation}
         onChangeText={setStartLocation}
       />
       <TextInput
+        style={styles.input}
         placeholder="Destination"
         value={destination}
         onChangeText={setDestination}
       />
-      {/* Add Preferences Modal or Section here */}
-      <Button
-        title="Search"
-        onPress={handleSearch}
-      />
+      <View style={styles.buttonContainer}>
+        <Button title = "Set Preferences" onPress = {openPreferences} color="#007AFF" />
+        <Button title = "Search" onPress = {handleSearch} color="#007AFF" />
       {/* Rest of the UI */}
+      </View>
     </View>
   );
 };
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    padding: 20,
+  },
+  input: {
+    height: 40,
+    borderColor: 'gray',
+    borderWidth: 1,
+    marginBottom: 10,
+    paddingHorizontal: 10,
+    borderRadius: 5,
+  },
+  buttonContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    marginTop: 20,
+  },
+  errorText: {
+    color: 'red',
+    marginBottom: 10,
+    textAlign: 'center',
+  },
+});
 
 export default NavigationPage;
