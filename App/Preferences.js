da41e7bfd1955e06c924f794ef8da66028cabe87
skak122 <71853125+skak122@users.noreply.github.com>
2023-11-10 19:23:14 -0800

    Update Preferences.js

--- App/Preferences.js
+++ App/Preferences.js
@@ -1,23 +1,35 @@
 import React, { useState } from 'react';
-import { View, TextInput, Button, StyleSheet } from 'react-native';
+import { View, TextInput, Button, StyleSheet, Alert } from 'react-native';
 import axios from 'axios';
+
+// Constants
+const PREFERENCES_URL = 'http://localhost:3000/user/preferences';
 
 const PreferencesForm = () => {
   const [budget, setBudget] = useState('');
   const [safetyPriority, setSafetyPriority] = useState('');
   const [speedPriority, setSpeedPriority] = useState('');
 
+  // Validate input as numeric
+  const isNumeric = (value) => /^[0-9]+(\.[0-9]+)?$/.test(value);
+
+  // Handle form submission
   const handleSubmit = async () => {
+    if (!isNumeric(budget) || !isNumeric(safetyPriority) || !isNumeric(speedPriority)) {
+      Alert.alert('Invalid Input', 'Please enter numeric values only.');
+      return;
+    }
+
     try {
-      await axios.put('http://localhost:3000/user/preferences', {
+      await axios.put(PREFERENCES_URL, {
         budget,
         safetyPriority,
         speedPriority
       });
-      alert('Preferences updated successfully!');
+      Alert.alert('Success', 'Preferences updated successfully!');
     } catch (error) {
       console.error('Error updating preferences:', error);
-      alert('Failed to update preferences');
+      Alert.alert('Error', 'Failed to update preferences. Please try again later.');
     }
   };
 
