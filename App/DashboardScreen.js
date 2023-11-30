6d46893abe25fee02ed7c14888ce8ce3b74b5bb2
naveengovind <36118508+naveengovind@users.noreply.github.com>
2023-11-30 11:33:51 -0800

    everything

--- App/DashboardScreen.js
+++ App/DashboardScreen.js
@@ -51,57 +51,93 @@ const Dashboard = () => {
     }
 }
 
+function parseRidesData(jsonData) {
+  // Checking if 'data' and 'products' exist in the JSON
+  if (!jsonData.data || !jsonData.data.products || !jsonData.data.products.tiers) {
+      console.log("Invalid JSON structure");
+      return;
+  }
+
+  const tiers = jsonData.data.products.tiers;
+  const rides = [];
+
+  // Looping through each tier
+  tiers.forEach(tier => {
+      // Checking if products exist in the tier
+      if (!tier.products) return;
+
+      // Looping through each product in the tier
+      tier.products.forEach(product => {
+          // Extracting required information
+          const rideType = product.displayName;
+          const estimatedTripTime = product.estimatedTripTime;
+          const fare = product.fare;
+
+          // Adding to the rides array
+          rides.push({
+              rideType,
+              estimatedTripTime,
+              fare
+          });
+      });
+  });
+
+  return rides;
+}
+
   async function getRidePrice(originLatitude, originLongitude, destinationLatitude, destinationLongitude) {
     var myHeaders = new Headers();
-myHeaders.append("authority", "m.uber.com");
-myHeaders.append("accept", "*/*");
-myHeaders.append("accept-language", "en-US,en;q=0.9");
-myHeaders.append("content-type", "application/json");
-myHeaders.append("cookie", "marketing_vistor_id=c546454c-59f9-4dc2-ab24-bde1e5f47d3c; segmentCookie=b; utag_main_segment=a; utag_geo_code=US; utag_main_optimizely_segment=a; UBER_CONSENTMGR=1701313539094|consent:true; CONSENTMGR=c1:1%7Cc2:1%7Cc3:1%7Cc4:1%7Cc5:1%7Cc6:1%7Cc7:1%7Cc8:1%7Cc9:1%7Cc10:1%7Cc11:1%7Cc12:1%7Cc13:1%7Cc14:1%7Cc15:1%7Cts:1701313539094%7Cconsent:true; udi-id=xPOviiR0bDIVkOGZi+Ju9wNbAo3c8Ihh4QzqCGw/Flm/GRLPkVXiUHsLEdRg82lHiMeeTERlR0Bjns/iQVpfD61ip524jK38Bf6QsJP0Bg+baM2ghziAef7UdoX6SnohHOLbNWyCKWMUGyQu7gLoJmackE5aghUA0yH5s8I0VSMQPrfIlAD3VDpcB+ClLowW27uozGvZ07hAe+ol79J39Q==uSgEzMFxOkGOAfhRMFAssw==AW6PsWbCW2l+NDqg5ro7stqkcn+m2R8KnDjUqyLLD9s=; sid=QA.CAESEOKcdekFkkAdo3DctptfSE8YkJK-rAYiATEqJDY0MTc4Mzc1LWJlMTAtNDcxOC1hOGYzLTk0OWJmY2JlZTA0NTI8RK5KVk_w9VMRDiv3G5Ih2f8MoChSlV9QKEdu6vC1CD9ojAdc0TddByW0kA_Eg0M-UnZo_-W-dtjzcxsuOgExQgh1YmVyLmNvbQ.XS06ksg-RVAlfQYAkFqnZOU9GGi5etgVnbBdHaevxKY; csid=1.1703905552407.zWpeMUxMrv7Ru92YI9A1CTTMG712TnhMlGLA7QQwHf0=; _cc=AaF0k89JYcVPsp5MsWaElimS; _cid_cc=AaF0k89JYcVPsp5MsWaElimS; udi-fingerprint=9mWRv3awsvECP4XF/GQJWeC39AXZZZmrnf1B7d3o+/AMtFmWSkJ+G6fsNmL5sCwSExIdilUkC8PrBP2AgLEJNQ==s4CMa6ALnTSH2pUKrenf4gqG+qlvh5h6SIxePuN4/GQ=; x-uber-analytics-session-id=79f7b817-2ce9-4051-9019-ff4b967e4050; utag_main__sn=2; utag_main_ses_id=1701333654159%3Bexp-session; utag_main__ss=0%3Bexp-session; _ua={\"session_id\":\"1a06ed7b-a99b-43c0-8cbb-9f9148fb79c6\",\"session_time_ms\":1701333678241}; utag_main__pn=3%3Bexp-session; utag_main__se=8%3Bexp-session; utag_main__st=1701335668616%3Bexp-session; jwt-session=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpYXQiOjE3MDEzMzM4NjksImV4cCI6MTcwMTQyMDI2OX0.v_FR3x0cfyEz5Hvn-7VDQU0tCuvVRGhVgs2NXeA_Gmw; marketing_vistor_id=c546454c-59f9-4dc2-ab24-bde1e5f47d3c; _ua={\"session_id\":\"1a06ed7b-a99b-43c0-8cbb-9f9148fb79c6\",\"session_time_ms\":1701333678241}");
-myHeaders.append("dnt", "1");
-myHeaders.append("origin", "https://m.uber.com");
-myHeaders.append("referer", "https://m.uber.com/go/product-selection?drop%5B0%5D=%7B%22addressLine1%22%3A%22340%20Fremont%20Apartments%22%2C%22addressLine2%22%3A%22340%20Fremont%20St%2C%20San%20Francisco%2C%20CA%22%2C%22id%22%3A%220300be20-8f73-0a67-1c26-cb68e8f7c46d%22%2C%22source%22%3A%22SEARCH%22%2C%22latitude%22%3A37.7870293%2C%22longitude%22%3A-122.3929983%2C%22provider%22%3A%22uber_places%22%7D&pickup=%7B%22addressLine1%22%3A%22Santa%20Clara%20Natural%20Organic%20Market%22%2C%22addressLine2%22%3A%22San%20Francisco%2C%20CA%22%2C%22id%22%3A%22e8ce16ce-f414-269b-c01f-4792de2307a8%22%2C%22source%22%3A%22SEARCH%22%2C%22latitude%22%3A37.771594%2C%22longitude%22%3A-122.435305%2C%22provider%22%3A%22uber_places%22%7D&uclick_id=30d0ed2d-7111-42df-867f-30d99485fe36&vehicle=8");
-myHeaders.append("sec-ch-ua", "\"Chromium\";v=\"119\", \"Not?A_Brand\";v=\"24\"");
-myHeaders.append("sec-ch-ua-mobile", "?0");
-myHeaders.append("sec-ch-ua-platform", "\"macOS\"");
-myHeaders.append("sec-fetch-dest", "empty");
-myHeaders.append("sec-fetch-mode", "cors");
-myHeaders.append("sec-fetch-site", "same-origin");
-myHeaders.append("user-agent", "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/119.0.0.0 Safari/537.36");
-myHeaders.append("x-csrf-token", "x");
-myHeaders.append("x-uber-recaptcha-token", "[\"03AFcWeA6zKW5wE-42UPWgE-r71AgRv6jAon-Uab08rmx6T5XleOkIR82it_IoWHww4mTNEFMTxDnt5iGGKnDeLZLieoRjqngoopS1bxciXVX457aJXwiuW9ZbLTzZCZ1RuNUyuwrq2QAfCh9nwz2ZlYdblgvz2wOL9YAMGPHWgELpSCS1PqeU24U41L1_cVsYydznm7J8uH0Z_9WCLVC-eQMNE8W54NdBB3ret0UpDMpNJvIbZ09bD2I-hJjbIYajcKPhDIHVcswbfvaXLR4CR4umkpTTl9j1lThp7bDClXFGm-KVCdg00wu7yg12i0W9o9UtwiZUkoRtS1W5tGPImaWGrXNKesE9uzD10otpO5Li-l8XcNdDwjZoisfc5FYHUHyMaKOhhnN_onEQzq2vyrBfsJNzg-izOUm_6V5OOOp8TlSm3iO9yvVpm_jCPMzjpnL29D98D_bw1i4ptnt5QY2qmwGpCA89HnMxPC4MDYGQUHjSiRQMmrRcCXkQfOAfJZRhhPBezRL-kT8ioQghA8pBaIyeZyXyCzLeHHNonAXSeTZw_NxbGQ13rWFDbE6Z2j-Azz9fCh8Mw02keSb_G80H1Nfv84firdPTMlM6zBOV8B9OPSgURylICnrbHz4JgbK0xzCOq2Vlj1te0xUSEopXI8eoNRsjx5pJuAM3qvPZOhKIjEgy_fvZXXkHf9gJwixhH5dw2z9ioBBLJXjchyUiNqc-QN1RDFzhe_7yNNq_-gnGBlTaVEq59fGYy1dpdte1wNPWAgTF7uGsZsRCU3eE556gXk_84H_UM6kytR2Rap8grapwU3JeQXcrsO9HAQtHzVSp8tl0XxU0R8jCi7RZG8rWYoF5x7inT6PoMg_2FkDOCdrrcCJzgr1SrDYFGxIFG6yU0sqLPnxHmILXk6JtJg7wRpHpw72KqZ1FYIT1A0jB_xTM4YDN_3Ab8vKCmbw84TG8mq4Zdl8NrO0Qb0lb3tuxQ5fFeZ_jcu6Wv9bPDCeeYEXQqyUXPtQczc_L2ke8EVYrg5RQeHbEX5OQGK8NrQEGbRNwEFc4tsglC0cFyI53qjn31s4xtz91amoF6xcjfdvJw8Y82doHxTQWCfOE9_EPke1oefxTR1WXC48YeeVI3DJPA6tR6VnALvNx3GvJtjkil-riqzv7sY-fWH7sc1R33zhE9OP9iJTMF9O45oPmpx7mSCRHikL9n81Y5KRVILExadyAjvo2cfj59s74QStwluTrteufj9N4SDinSwI8p2GS4sr5pRr3OzwhOvx_bNKyIVvhY2MJNxmeo1TKxPEqnnghq2SqLTciPyBWsUGP2LMO7btNgjelQpqEvAdeuZ0esomBFm79XOWg6abF5XXsCCEfJLshsXWNMYgvuAPFU0H_g0iCOkyguzkom1zf31DamWx1pNUbGeXjk_z0E6ZrseGBLETXnGgPyYFKHX__jQA8jK3iH2SEAIXv5sTvzK6hDQJ1a08ThEH3vM3Qns38JfIWRgB6LClmqFmYGSPc9OBvOsanKtL_qpYVnw71EOb7QHDKjf-Iz_w9paxb8w0HvN7N62YEbUILWS8MFwgGFRSpy9lfBLTrfv9p41n1MN7muCXcbu9cAIMUNr2y5zJABasRe3g4OLt9RUow95ncRhWd1wi2CNtP0xVL5wP-Ur4ev7j1wtR-OF8CJ3O_3Wr933A5hK2k84EE6J2wNA5VfuWDES2gKRHJl2CrzIIPgKowjV4glL0vWWkWkLHOC95A85SWFqP1-qtMEIfQkbSqAC4gSf-0mEiMGMI3sZkh6xUS3Y23a5lE8geSg5xCHsxbMgHWGR2yFZ9l-XRtF4VzEZp8v77DW11QxocE1fgs8KK7tmn0N6Z_G1Aq_lr249hIgCW2Dai8FnJF-4eBN6ZK8H7-od01MJg7n4dFiMWwq1rmZ8p-OGs6vIYtrAj5F6sXgTtRyQ\",969,\"resolved\"]");
-myHeaders.append("x-uber-rv-session-type", "desktop_session");
-myHeaders.append("x-uber-rv-tenancy", "uber/production");
+    myHeaders.append("authority", "m.uber.com");
+    myHeaders.append("accept", "*/*");
+    myHeaders.append("accept-language", "en-US,en;q=0.9");
+    myHeaders.append("content-type", "application/json");
+    myHeaders.append("cookie", "marketing_vistor_id=c546454c-59f9-4dc2-ab24-bde1e5f47d3c; segmentCookie=b; utag_main_segment=a; utag_geo_code=US; utag_main_optimizely_segment=a; UBER_CONSENTMGR=1701313539094|consent:true; CONSENTMGR=c1:1%7Cc2:1%7Cc3:1%7Cc4:1%7Cc5:1%7Cc6:1%7Cc7:1%7Cc8:1%7Cc9:1%7Cc10:1%7Cc11:1%7Cc12:1%7Cc13:1%7Cc14:1%7Cc15:1%7Cts:1701313539094%7Cconsent:true; udi-id=xPOviiR0bDIVkOGZi+Ju9wNbAo3c8Ihh4QzqCGw/Flm/GRLPkVXiUHsLEdRg82lHiMeeTERlR0Bjns/iQVpfD61ip524jK38Bf6QsJP0Bg+baM2ghziAef7UdoX6SnohHOLbNWyCKWMUGyQu7gLoJmackE5aghUA0yH5s8I0VSMQPrfIlAD3VDpcB+ClLowW27uozGvZ07hAe+ol79J39Q==uSgEzMFxOkGOAfhRMFAssw==AW6PsWbCW2l+NDqg5ro7stqkcn+m2R8KnDjUqyLLD9s=; sid=QA.CAESEOKcdekFkkAdo3DctptfSE8YkJK-rAYiATEqJDY0MTc4Mzc1LWJlMTAtNDcxOC1hOGYzLTk0OWJmY2JlZTA0NTI8RK5KVk_w9VMRDiv3G5Ih2f8MoChSlV9QKEdu6vC1CD9ojAdc0TddByW0kA_Eg0M-UnZo_-W-dtjzcxsuOgExQgh1YmVyLmNvbQ.XS06ksg-RVAlfQYAkFqnZOU9GGi5etgVnbBdHaevxKY; csid=1.1703905552407.zWpeMUxMrv7Ru92YI9A1CTTMG712TnhMlGLA7QQwHf0=; _cc=AaF0k89JYcVPsp5MsWaElimS; _cid_cc=AaF0k89JYcVPsp5MsWaElimS; udi-fingerprint=9mWRv3awsvECP4XF/GQJWeC39AXZZZmrnf1B7d3o+/AMtFmWSkJ+G6fsNmL5sCwSExIdilUkC8PrBP2AgLEJNQ==s4CMa6ALnTSH2pUKrenf4gqG+qlvh5h6SIxePuN4/GQ=; x-uber-analytics-session-id=79f7b817-2ce9-4051-9019-ff4b967e4050; utag_main__sn=2; utag_main_ses_id=1701333654159%3Bexp-session; utag_main__ss=0%3Bexp-session; _ua={\"session_id\":\"1a06ed7b-a99b-43c0-8cbb-9f9148fb79c6\",\"session_time_ms\":1701333678241}; utag_main__pn=3%3Bexp-session; utag_main__se=8%3Bexp-session; utag_main__st=1701335668616%3Bexp-session; jwt-session=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpYXQiOjE3MDEzMzM4NjksImV4cCI6MTcwMTQyMDI2OX0.v_FR3x0cfyEz5Hvn-7VDQU0tCuvVRGhVgs2NXeA_Gmw; marketing_vistor_id=c546454c-59f9-4dc2-ab24-bde1e5f47d3c; _ua={\"session_id\":\"1a06ed7b-a99b-43c0-8cbb-9f9148fb79c6\",\"session_time_ms\":1701333678241}");
+    myHeaders.append("dnt", "1");
+    myHeaders.append("origin", "https://m.uber.com");
+    myHeaders.append("referer", "https://m.uber.com/go/product-selection?drop%5B0%5D=%7B%22addressLine1%22%3A%22340%20Fremont%20Apartments%22%2C%22addressLine2%22%3A%22340%20Fremont%20St%2C%20San%20Francisco%2C%20CA%22%2C%22id%22%3A%220300be20-8f73-0a67-1c26-cb68e8f7c46d%22%2C%22source%22%3A%22SEARCH%22%2C%22latitude%22%3A37.7870293%2C%22longitude%22%3A-122.3929983%2C%22provider%22%3A%22uber_places%22%7D&pickup=%7B%22addressLine1%22%3A%22Santa%20Clara%20Natural%20Organic%20Market%22%2C%22addressLine2%22%3A%22San%20Francisco%2C%20CA%22%2C%22id%22%3A%22e8ce16ce-f414-269b-c01f-4792de2307a8%22%2C%22source%22%3A%22SEARCH%22%2C%22latitude%22%3A37.771594%2C%22longitude%22%3A-122.435305%2C%22provider%22%3A%22uber_places%22%7D&uclick_id=30d0ed2d-7111-42df-867f-30d99485fe36&vehicle=8");
+    myHeaders.append("sec-ch-ua", "\"Chromium\";v=\"119\", \"Not?A_Brand\";v=\"24\"");
+    myHeaders.append("sec-ch-ua-mobile", "?0");
+    myHeaders.append("sec-ch-ua-platform", "\"macOS\"");
+    myHeaders.append("sec-fetch-dest", "empty");
+    myHeaders.append("sec-fetch-mode", "cors");
+    myHeaders.append("sec-fetch-site", "same-origin");
+    myHeaders.append("user-agent", "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/119.0.0.0 Safari/537.36");
+    myHeaders.append("x-csrf-token", "x");
+    myHeaders.append("x-uber-rv-session-type", "desktop_session");
+    myHeaders.append("x-uber-rv-tenancy", "uber/production");
 
-var raw = JSON.stringify({
-  "operationName": "Products",
-  "variables": {
-    "includeRecommended": false,
-    "destinations": [
-      {
-        "latitude": 37.7870293,
-        "longitude": -122.3929983
-      }
-    ],
-    "pickup": {
-      "latitude": 37.771594,
-      "longitude": -122.435305
-    }
-  },
-  "query": "query Products($destinations: [InputCoordinate!]!, $includeRecommended: Boolean = false, $pickup: InputCoordinate!, $pickupFormattedTime: String, $profileType: String, $targetProductType: EnumRVWebCommonTargetProductType) {\n  products(\n    destinations: $destinations\n    includeRecommended: $includeRecommended\n    pickup: $pickup\n    pickupFormattedTime: $pickupFormattedTime\n    profileType: $profileType\n    targetProductType: $targetProductType\n  ) {\n    ...ProductsFragment\n    __typename\n  }\n}\n\nfragment ProductsFragment on RVWebCommonProductsResponse {\n  classificationFilters {\n    ...ClassificationFiltersFragment\n    __typename\n  }\n  defaultVVID\n  productsUnavailableMessage\n  renderRankingInformation\n  tiers {\n    ...TierFragment\n    __typename\n  }\n  __typename\n}\n\nfragment BadgesFragment on RVWebCommonProductBadge {\n  color\n  text\n  __typename\n}\n\nfragment ClassificationFiltersFragment on RVWebCommonClassificationFilters {\n  filters {\n    ...ClassificationFilterFragment\n    __typename\n  }\n  hiddenVVIDs\n  standardProductVVID\n  __typename\n}\n\nfragment ClassificationFilterFragment on RVWebCommonClassificationFilter {\n  currencyCode\n  displayText\n  fareDifference\n  icon\n  vvid\n  __typename\n}\n\nfragment TierFragment on RVWebCommonProductTier {\n  products {\n    ...ProductFragment\n    __typename\n  }\n  title\n  __typename\n}\n\nfragment ProductFragment on RVWebCommonProduct {\n  badges {\n    ...BadgesFragment\n    __typename\n  }\n  capacity\n  cityID\n  currencyCode\n  description\n  detailedDescription\n  discountPrimary\n  displayName\n  estimatedTripTime\n  etaStringShort\n  fare\n  fareAmountE5\n  hasPromo\n  hasRidePass\n  id\n  is3p\n  isAvailable\n  legalConsent {\n    ...ProductLegalConsentFragment\n    __typename\n  }\n  meta\n  preAdjustmentValue\n  productImageUrl\n  productUuid\n  reserveEnabled\n  __typename\n}\n\nfragment ProductLegalConsentFragment on RVWebCommonProductLegalConsent {\n  header\n  image {\n    url\n    width\n    __typename\n  }\n  description\n  enabled\n  ctaUrl\n  ctaDisplayString\n  buttonLabel\n  showOnce\n  __typename\n}\n"
-});
+    var raw = JSON.stringify({
+      "operationName": "Products",
+      "variables": {
+        "includeRecommended": false,
+        "destinations": [
+          {
+            "latitude": 37.7870293,
+            "longitude": -122.3929983
+          }
+        ],
+        "pickup": {
+          "latitude": 37.771594,
+          "longitude": -122.435305
+        }
+      },
+      "query": "query Products($destinations: [InputCoordinate!]!, $includeRecommended: Boolean = false, $pickup: InputCoordinate!, $pickupFormattedTime: String, $profileType: String, $targetProductType: EnumRVWebCommonTargetProductType) {\n  products(\n    destinations: $destinations\n    includeRecommended: $includeRecommended\n    pickup: $pickup\n    pickupFormattedTime: $pickupFormattedTime\n    profileType: $profileType\n    targetProductType: $targetProductType\n  ) {\n    ...ProductsFragment\n    __typename\n  }\n}\n\nfragment ProductsFragment on RVWebCommonProductsResponse {\n  classificationFilters {\n    ...ClassificationFiltersFragment\n    __typename\n  }\n  defaultVVID\n  productsUnavailableMessage\n  renderRankingInformation\n  tiers {\n    ...TierFragment\n    __typename\n  }\n  __typename\n}\n\nfragment BadgesFragment on RVWebCommonProductBadge {\n  color\n  text\n  __typename\n}\n\nfragment ClassificationFiltersFragment on RVWebCommonClassificationFilters {\n  filters {\n    ...ClassificationFilterFragment\n    __typename\n  }\n  hiddenVVIDs\n  standardProductVVID\n  __typename\n}\n\nfragment ClassificationFilterFragment on RVWebCommonClassificationFilter {\n  currencyCode\n  displayText\n  fareDifference\n  icon\n  vvid\n  __typename\n}\n\nfragment TierFragment on RVWebCommonProductTier {\n  products {\n    ...ProductFragment\n    __typename\n  }\n  title\n  __typename\n}\n\nfragment ProductFragment on RVWebCommonProduct {\n  badges {\n    ...BadgesFragment\n    __typename\n  }\n  capacity\n  cityID\n  currencyCode\n  description\n  detailedDescription\n  discountPrimary\n  displayName\n  estimatedTripTime\n  etaStringShort\n  fare\n  fareAmountE5\n  hasPromo\n  hasRidePass\n  id\n  is3p\n  isAvailable\n  legalConsent {\n    ...ProductLegalConsentFragment\n    __typename\n  }\n  meta\n  preAdjustmentValue\n  productImageUrl\n  productUuid\n  reserveEnabled\n  __typename\n}\n\nfragment ProductLegalConsentFragment on RVWebCommonProductLegalConsent {\n  header\n  image {\n    url\n    width\n    __typename\n  }\n  description\n  enabled\n  ctaUrl\n  ctaDisplayString\n  buttonLabel\n  showOnce\n  __typename\n}\n"
+    });
 
-var requestOptions = {
-  method: 'POST',
-  headers: myHeaders,
-  body: raw,
-  redirect: 'follow'
-};
+    var requestOptions = {
+      method: 'POST',
+      headers: myHeaders,
+      body: raw,
+      redirect: 'follow'
+    };
 
-fetch("https://m.uber.com/go/graphql", requestOptions)
-  .then(response => response.text())
-  .then(result => console.log(result))
-  .catch(error => console.log('error', error));
+    return fetch("https://m.uber.com/go/graphql", requestOptions)
+      .then(response => response.json())
+      .then(result => {
+        console.log(result)
+        return parseRidesData(result);
+      })
+      .catch(error => console.log('error', error));
   }
 
   const fetchAddressSuggestions = async (query, setSuggestions) => {
