4bd8c5f4a2606a1c573c64cb2759b13d62aea8f3
tspython <tspython@hotmail.com>
2023-12-01 03:50:35 -0800

    add rec algo

--- App/RecAlgo.js
+++ App/RecAlgo.js
@@ -1,8 +1,8 @@
 const baseSafetyRatings = {
   "UberX": 16,
   "Uber Green": 16,
-  "UberXL": 16,
   "Comfort Electric": 16,
+  "UberXL": 16,
   "Comfort": 16,
   "Uber Pet": 16,
   "Black": 16,
@@ -13,33 +13,90 @@ const baseSafetyRatings = {
   "Lyft": 16,
   "Extra Comfort": 16,
   "XL": 16,
-  "Black": 16,
-  "Black SUV": 16,
-  "VTA": 13,
+  "BUS": 13,
   "BART": 10,
   "Caltrain": 18
 };
 
-const calculateSafetyRating = (cost, safety, speed, transportationOptions) => {
-  let adjustedSafetyRatings = {};
+function rankOptions(cost, safety, speed, transportationData) {
+  const rankedOptions = transportationData.map(option => {
+    const safetyDifference = Math.abs(option.safety - safety);
+    const costDifference = Math.abs(option.cost - cost);
+    const speedDifference = Math.max(0, Math.abs(option.speedRating - speed)); // Adjusted to handle speed below 0
 
-  for (let option of transportationOptions) {
-    const time = option.time;
-    const distance = option.distance;
-    const distanceTimeFactor = (time * distance) / 1000;
+    const totalDifference = safetyDifference + costDifference + speedDifference;
 
-    let scaledSafety = baseSafetyRatings[option.type] - distanceTimeFactor * 0.1;
-    scaledSafety -= (20 - safety) * 0.2;
-    scaledSafety += speed * 0.5;
+    return { option, totalDifference };
+  });
 
-    if (cost < option.priceRange.low || cost > option.priceRange.high) {
-      scaledSafety -= 0.5;
-    }
+  // Sort based on the total difference
+  rankedOptions.sort((a, b) => a.totalDifference - b.totalDifference);
 
-    adjustedSafetyRatings[option.type] = scaledSafety;
+  // Scale the safety score of each option using adjustSafety function
+  rankedOptions.forEach((rankedOption) => {
+    rankedOption.option.safety = adjustSafety(rankedOption.option.safety, rankedOption.option.distance, rankedOption.option.time);
+  });
+
+  return rankedOptions;
+}
+
+
+function adjustSafety(baseSafety, distance, time) {
+  const distanceFactor = 0.1;
+  const timeFactor = 0.05;
+
+  const safetyReduction = distance * distanceFactor + time * timeFactor;
+
+  let adjustedSafety = baseSafety - safetyReduction;
+
+  if (adjustedSafety < 0) {
+    adjustedSafety = 0;
   }
 
-  return adjustedSafetyRatings;
-};
+  return adjustedSafety;
+}
 
-export default calculateSafetyRating;
+function parseTransportationOptions(lyftData, uberData, publicTransitData) {
+  const transportationOptions = [];
+
+    // Parse Uber data and convert estimatedTripTime from seconds to minutes
+  const formattedUberData = uberData.map((ride) => ({
+    type: ride.rideType,
+    cost: parseFloat(ride.fare.replace('$', '')),
+    safety: baseSafetyRatings[ride.rideType] || 16, // Default safety value as 16 for missing safety values
+    distance: parseFloat(ride.distanceInMiles), // Use distanceInMiles attribute for distance
+    time: Math.ceil(ride.estimatedTripTime / 60), // Convert seconds to minutes
+    speed: 0, // Initial speed value set to 0
+  }));
+  transportationOptions.push(...formattedUberData);
+  // Parse Public Transit data
+  const publicTransitOption = {
+    type: publicTransitData.legs[0].transitDetails[0].vehicleType,
+    cost: parseFloat(publicTransitData.fare.replace('$', '').split(' ')[0]),
+    safety: 16, // Default safety value as 16 for Public Transit
+    distance: parseFloat(publicTransitData.totalDistance.split(' ')[0]), // Extract distance in miles
+    time: parseInt(publicTransitData.totalDuration.split(' ')[0]), // Extract minutes from totalDuration
+    speed: 0, // Initial speed value set to 0
+  };
+
+
+  transportationOptions.push(publicTransitOption);
+  transportationOptions.forEach((option) => {
+   // if (option.speed === 0) {
+    //  option.speed = calculateSpeed(option.distance, option.time, transportationOptions);
+    //}
+    // Calculating rating for speed based on time
+    option.speedRating = calculateSpeedRatingBasedOnTime(option.time, transportationOptions);
+  });
+
+  return transportationOptions;
+}
+
+function calculateSpeedRatingBasedOnTime(time, allOptions) {
+  const times = allOptions.map((option) => option.time).sort((a, b) => a - b);
+  const index = times.indexOf(time);
+  const rating = time > 0 ? ((times.length - index - 1) / (times.length - 1)) * 5 : 0;
+  return rating;
+}
+
+export default rankOptions;
